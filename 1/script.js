99443afd739372c17982eb5b25b060df103a2bca
akyumi <127708902+akyumi@users.noreply.github.com>
2023-03-13 11:21:49 +0800

    Add files via upload

---
+++ 1/script.js
@@ -0,0 +1,13 @@
+let i = 0;
+while (i < 60) {
+    document.writeln(i+=2);
+}
+document.write("<br>");
+let x = 0;
+do {
+    document.writeln(x+=3);
+} while (x < 60);
+document.write("<br>");
+for (let l =5; l <= 50; l+=5) {
+    document.writeln(l);
+}
